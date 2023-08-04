7fe01d0c92c772abb09b43011bf3d50db808ba83
FdlyLol <131168637+FdlyMots@users.noreply.github.com>
2023-08-05 06:48:04 +0700

    Update status.sh

--- status.sh
+++ status.sh
@@ -132,8 +132,8 @@ clear;clear;clear
 
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo -e "             ${YELLOW}(${NC}${GREEN} STATUS SERVICE INFORMATION ${NC}${YELLOW})${NC}"
-echo -e "           OWNER : ${GREEN} BTR ${NC}${YELLOW}(${NC} ${GREEN} SC BY ANDY YUDA  ${NC}${YELLOW})${NC}"
-echo -e "       © Copyright By ANDY TUNNEL ${YELLOW}(${NC} 2022-2023 ${YELLOW})${NC}"
+echo -e "           OWNER : ${GREEN} Alpha-zw ${NC}${YELLOW}(${NC} ${GREEN} ZwOfc  ${NC}${YELLOW})${NC}"
+echo -e "       © Copyright By ZwOfc ${YELLOW}(${NC} 2022-2023 ${YELLOW})${NC}"
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
 echo -e "${RED_BG}                     Sytem Information                    ${NC}"
